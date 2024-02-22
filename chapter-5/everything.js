7b75099b6ab9ca37bee55cfd3a8c38231ab0c862
Abadur Rahman Imtiaz <83642378+arimtiaz@users.noreply.github.com>
2024-02-22 15:04:32 +0600

    Chapter-5

---
+++ chapter-5/everything.js
@@ -0,0 +1,11 @@
+function every(array, predicate) {
+    for (let element of array) {
+      if (!predicate(element)) return false;
+    }
+    return true;
+  }
+  
+  function every2(array, predicate) {
+    return !array.some(element => !predicate(element));
+  }
+  
